ac4d82f8e91e5c217f037b868782da431bc945fb
garcie67 <ernie1994@gmail.com>
2019-06-24 21:59:41 -0700

    Added recipe model and api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,64 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+module.exports = function (app) {
+
+  app.get("/api/recipes", function (req, res) {
+    db.Recipe.findAll({}).then(function (results) {
+      res.json(results);
     });
+  });
+
+  app.get("/api/recipes/:field/:mode", function (req, res) {
+
+    //field is name of db field
+    //field is preferably 'name' or 'rating'
+    //mode is either ASC or DESC
+
+    db.Recipe.findAll({
+
+      order: [
+        [req.params.field, req.params.mode]
+      ]
+
+    }).then(function (results) {
+      res.json(results);
+    })
+
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/recipes", function (req, res) {
+    db.Recipe.create(req.body).then(function (results) {
+      res.json(results);
     });
   });
 
+  app.put("/api/recipes/:id", function (req, res) {
+
+    //insert error handling if id is not a valid int
+    var rating = parseInt(req.body.rating);
+
+    var recipeId = parseInt(req.params.id);
+
+    db.Recipe.findOne({
+      where: { id: recipeId }
+    })
+      .then(function (results) {
+
+        db.Recipe.update(
+          { rating: results.rating + rating },
+          { where: { id: recipeId } },
+        )
+          .then(function (updateResults) {
+            res.json(updateResults);
+          });
+      });
+  });
+
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  app.delete("/api/recipes/:id", function (req, res) {
+    db.Recipe.destroy({ where: { id: req.params.id } }).then(function (results) {
+      res.json(results);
     });
   });
 };
